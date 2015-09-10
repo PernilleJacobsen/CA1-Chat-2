51e8918c67f17b4ba3124e9b65df6dfe8f284e59
Marta <marta.miszczyk@gmail.com>
2015-09-10 14:41:21 +0200

    Vi er færdig med jUnitTest

--- test/test/TestChatServer.java
+++ test/test/TestChatServer.java
@@ -32,13 +32,19 @@ public class TestChatServer
 
     private BufferedReader inA;
     private BufferedReader inB;
+     private BufferedReader inC;
     private PrintWriter outA;
     private PrintWriter outB;
+     private PrintWriter outC;
     private Socket socketA;
     private Socket socketB;
+    private Socket socketC;
     private final String userNameA = "USER#A";
     private final String userNameB = "USER#B";
+    private final String userNameC = "USER#C";
     private String msg;
+    private String stop="STOP#";
+    
 
     public TestChatServer()
     {
@@ -93,12 +99,98 @@ public class TestChatServer
         socketB = new Socket("localhost", 9090);
         inB = new BufferedReader(new InputStreamReader(socketB.getInputStream()));
         outB = new PrintWriter(socketB.getOutputStream(), true);
+        
+        socketC = new Socket("localhost", 9090);
+        inC = new BufferedReader(new InputStreamReader(socketC.getInputStream()));
+        outC = new PrintWriter(socketC.getOutputStream(), true);
+        
+        outA.println(userNameA);
+        outB.println(userNameB);
+        outC.println(userNameC);
+      
+        outA.println(msg);
+        assertEquals("MSG#A#JUnitTest", inA);
+        assertEquals("MSG#A#JUnitTest", inB);
+        assertEquals("MSG#A#JUnitTest", inC);
+        
+        outA.println(stop);
+        outB.println(stop);
+        outC.println(stop);
+      
+    }
+    
+    public void sendMSGtoTwo() throws IOException
+    {
+        //Vi test at vi kan sende til to 
+        msg = "MSG#B,C#JUnitTest";
+                
+        socketA = new Socket("localhost", 9090);
+        inA = new BufferedReader(new InputStreamReader(socketA.getInputStream()));
+        outA = new PrintWriter(socketA.getOutputStream(), true);
+
+        socketB = new Socket("localhost", 9090);
+        inB = new BufferedReader(new InputStreamReader(socketB.getInputStream()));
+        outB = new PrintWriter(socketB.getOutputStream(), true);
+        
+        socketC = new Socket("localhost", 9090);
+        inC = new BufferedReader(new InputStreamReader(socketC.getInputStream()));
+        outC = new PrintWriter(socketC.getOutputStream(), true);
+        
+        outA.println(userNameA);
+        outB.println(userNameB);
+        outC.println(userNameC);
+        
+        outA.println(msg);
+        assertEquals("MSG#A#JUnitTest", inB);
+        assertEquals("MSG#A#JUnitTest", inC);
+        
+        outA.println(stop);
+        outB.println(stop);
+        outC.println(stop);
+    }
+    
+    public void sendMSGtoOne() throws IOException
+    {
+        //Vi test at vi kan sende til en 
+        msg = "MSG#B#JUnitTest";
+                
+        socketA = new Socket("localhost", 9090);
+        inA = new BufferedReader(new InputStreamReader(socketA.getInputStream()));
+        outA = new PrintWriter(socketA.getOutputStream(), true);
+
+        socketB = new Socket("localhost", 9090);
+        inB = new BufferedReader(new InputStreamReader(socketB.getInputStream()));
+        outB = new PrintWriter(socketB.getOutputStream(), true);
         outA.println(userNameA);
         outB.println(userNameB);
         
         outA.println(msg);
-        assertEquals("MSG#A#JUnitTest", inA);
         assertEquals("MSG#A#JUnitTest", inB);
+        
+        outA.println(stop);
+        outB.println(stop);
+               
+    }
+    
+    public void userListStop() throws IOException
+    {
+        // Vi tester userlist og stop
+        
+        socketA = new Socket("localhost", 9090);
+        inA = new BufferedReader(new InputStreamReader(socketA.getInputStream()));
+        outA = new PrintWriter(socketA.getOutputStream(), true);
+
+        socketB = new Socket("localhost", 9090);
+        inB = new BufferedReader(new InputStreamReader(socketB.getInputStream()));
+        outB = new PrintWriter(socketB.getOutputStream(), true);
+        
+        outA.println(userNameA);
+        outB.println(userNameB);
+        
+        outA.println(stop);
+        assertEquals("USERLIST#B,", inB);
+        outB.println(stop);
+           
     }
     
     
