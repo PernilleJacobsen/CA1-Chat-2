d8a37135a03d41b25063aff9d4f14c358c1ab0d2
cph57689 <pernillejaco@gmail.com>
2015-09-10 08:51:34 +0200

    kopieret vores chat prjekt over i chat2 da vi fejlagtigt havde oprettet chat som webprojekt....

---
+++ src/server/ChatServer.java
@@ -0,0 +1,148 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.Utils;
+
+/**
+ *
+ * @author Pernille
+ */
+public class ChatServer
+{
+
+    private ServerSocket serverSocket;
+    private static final Properties properties = Utils.initProperties("server.properties");
+    private BufferedReader in;
+    private PrintWriter out;
+    private static boolean keepRunning = true;
+    private ClientHandler ch;
+    private Socket socket;
+    private final ConcurrentMap<String, ClientHandler> clients = new ConcurrentHashMap();
+    String[] splitInput = new String[100];
+
+    public static void stopServer()
+    {
+        keepRunning = false;
+    }
+
+    private void runServer(ChatServer cs)
+    {
+        int port = Integer.parseInt(properties.getProperty("port"));
+        String ip = properties.getProperty("serverIp");
+
+        Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, "Sever started. Listening on: " + port + ", bound to: " + ip);
+        try
+        {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(ip, port));
+            int clientsSize = 0;
+
+            while (keepRunning)
+            {
+                socket = serverSocket.accept(); //Important Blocking call
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, "Connected to a client");
+                out.println("Connected to a client, welcome");
+                String inputToSplit = in.readLine();
+                splitInput = inputToSplit.split("#");
+                String command = splitInput[0];
+                if (command.equals("USER"))
+                {
+                    String userName = splitInput[1];
+                    clients.put(userName, ch = new ClientHandler(socket, userName, cs));
+                    out.println("Welcome: " + userName);
+                    ch.start();
+                }
+                if (clientsSize!=clients.size())
+                {
+                    clientsSize = clients.size();
+                    cs.userList();
+                }
+
+            }
+        } catch (IOException ex)
+        {
+            Logger.getLogger(ChatServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String logFile = properties.getProperty("logFile");
+        Utils.setLogFile(logFile, ChatServer.class.getName());
+
+        try
+        {
+            ChatServer cs = new ChatServer();
+            cs.runServer(cs);
+        } finally
+        {
+            Utils.closeLogger(ChatServer.class.getName());
+        }
+
+    }
+
+    public void sendToAll(String msg)
+    {
+        System.out.println("Hvad er det her: " + msg);
+        System.out.println("5.client size er: " + clients.size());
+        for (Map.Entry<String, ClientHandler> entry : clients.entrySet())
+        {
+            System.out.println("kan vi komme her ned?");
+            ClientHandler receiver = entry.getValue();
+            receiver.sendMSG(msg);
+        }
+    }
+
+    public void sendToSome(String msg, String[] receivers)
+    {
+        for (String receiver1 : receivers)
+        {
+            String temp = receiver1;
+            if (clients.containsKey(receiver1))
+            {
+                System.out.println("receiver 1 i client: " + clients.get(temp));
+                ClientHandler receiver = clients.get(temp);
+                receiver.sendMSG(msg);
+            }
+        }
+    }
+
+    public void sendToOne(String msg, String receivers)
+    {
+        ClientHandler receiver = clients.get(receivers);
+        receiver.sendMSG(msg);
+    }
+
+    public void removeUser(String userName)
+    {
+        clients.remove(userName);
+    }
+
+    public void userList()
+    {
+        String temp;
+        String msg = "USERLIST#";
+        ClientHandler receiver = null;
+        for (Map.Entry<String, ClientHandler> entry : clients.entrySet())
+        {
+            temp = entry.getKey() ;
+            receiver = entry.getValue();
+            msg += temp + ", ";
+        }
+        out.println(msg);
+    }
+}
---
+++ src/server/ClientHandler.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Pernille
+ */
+public class ClientHandler extends Thread
+{
+
+    Scanner input;
+    PrintWriter out;
+    Socket socket;
+    String userName;
+    ChatServer cs;
+
+    public ClientHandler(Socket socket, String userName, ChatServer cs) throws IOException
+    {
+        input = new Scanner(socket.getInputStream());
+        out = new PrintWriter(socket.getOutputStream(), true);
+        this.userName = userName;
+        this.cs = cs;
+        this.socket = socket;
+
+    }
+
+    public void sendMSG(String msg)
+    {
+        out.println(msg);
+    }
+
+    @Override
+    public void run()
+    {
+        String message = input.nextLine(); //IMPORTANT blocking call
+        Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, String.format("Received the message: %1$S ", message));
+
+        while (!message.equals("STOP#"))
+        {
+            String inputToSplit = message;
+            String[] splitInput = inputToSplit.split("#");
+            String command = splitInput[0];
+            if (command.equals("MSG"))
+            {
+                String[] receivers = splitInput[1].split(",");
+                System.out.println("recievers size er: " + receivers.length);
+                if (receivers.length == 1 && receivers[0].equals("*"))
+                {
+                    String msg = splitInput[2];
+                    cs.sendToAll(msg);
+                } else if (receivers.length == 1)
+                {
+                    //Vi vil gerne have fat i den clienthandler som navnet i receivers hør sammem
+                    //med.
+                    String msg = splitInput[2];
+                    String receiver = receivers[0];
+                    cs.sendToOne(msg, receiver);
+                } else if (receivers.length > 1)
+                {
+                    String msg = splitInput[2];
+                    cs.sendToSome(msg, receivers);
+                } else
+                {
+                    sendMSG("Please use one of the following commands:");
+                    sendMSG("MSG#USER1#...");
+                    sendMSG("MSG#USER1,USER2#...");
+                    sendMSG("MSG#*#...");
+                }
+            }
+            Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, String.format("Received the message: %1$S ", message.toUpperCase()));
+            message = input.nextLine(); //IMPORTANT blocking call
+        }
+        out.println("Bye bye");//Echo the stop message back to the client for a nice closedown
+        try
+        {
+            //Send userlist og fjern bruger fra map.
+            cs.removeUser(userName);
+            cs.userList();
+            currentThread().interrupt();
+            input.close();
+            out.close();
+            socket.close();
+        } catch (IOException ex)
+        {
+            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, "Closed a Connection");
+    }
+}
